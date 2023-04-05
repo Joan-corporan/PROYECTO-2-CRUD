660367e83b1219a8973ff5021331dedf3642f97d
Joan Corporan <joan.corporan.h@gmail.com>
2023-04-05 09:49:43 -0400

    Definiendo una constante btnREst, obteniendo su elemento id

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+
+/* DEFINIENDO UNA CONSTANTE "btnREst", OBTENIENDO SU ID "submit",
+ AÑADIENDO UN addEventListener al evento "click" con el nombre de la funcion  */
+const btnRest = document.getElementById("submit")
+btnRest.addEventListener("click", create)
+

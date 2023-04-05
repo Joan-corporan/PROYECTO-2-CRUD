2beb912582fb2cc036a5e53fc2c72bd2b0f27c1b
Joan Corporan <joan.corporan.h@gmail.com>
2023-04-05 10:04:04 -0400

    Definiendo un array vacio como una constante

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 
 /* DEFINIENDO UNA CONSTANTE "btnREst", OBTENIENDO SU ID "submit",
- AÑADIENDO UN addEventListener al evento "click" con el nombre de la funcion  */
+ AÑADIENDO UN addEventListener al evento "click" con el nombre de la funcion */
 const btnRest = document.getElementById("submit")
 btnRest.addEventListener("click", create)
+
+const tareas = []
 
